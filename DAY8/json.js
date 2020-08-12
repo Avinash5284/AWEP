a2e06168bb12634511eacb89835e18f9a98b51eb
Avinash5284 <65229426+Avinash5284@users.noreply.github.com>
2020-08-12 22:31:15 +0530

    json assignment

---
+++ DAY8/json.js
@@ -0,0 +1,47 @@
+let person ={
+    firstname:"Avinash",
+    
+    }
+    
+    
+    undefined
+    person;
+    {firstname: "Avinash"}
+
+
+    let demo = {
+
+        "first-name":"avinash",
+        }
+        undefined
+        demo;
+        {first-name: "avinash"}
+
+
+//list//
+ let list = [{ id : 10,firstname:'avinash'},
+{ id : 20,firstname:'avi'},
+{ id : 30,firstname:'shinde'}]
+
+
+
+undefined
+list;
+(3) [{…}, {…}, {…}]0: {id: 10, firstname: "avinash"}1: {id: 20, firstname: "avi"}2: {id: 30, firstname: "shinde"}length: 3__proto__: Array(0)
+let list = [{ id : 10,firstname:'avinash'},
+{ id : 20,firstname:'avi'},
+{ id : 30,firstname:'shinde'}]
+
+
+
+undefined
+let person ={id:1,name:"sagar"}
+undefined
+let list =[person["id"];
+           
+VM478:1 Uncaught SyntaxError: Unexpected token ';'
+let list =[person];
+           
+undefined
+list;
+ {id: 1, name: "sagar"}
